992a5437982f5519026e7898f63a922c7125281c
AIRASSART64 <irassarta@gmail.com>
2026-01-30 16:30:51 +0100

    auth_connection_second_commit

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getInviteController, loginController } = require('../controllers/authController.js');
-const { inviteUserController } = require('../controllers/adminController.js');
+const inviteUserController  = require('../controllers/adminController.js');
 
 
-router.post('/admin/invit', getInviteController);
+router.post('/admin/invit', inviteUserController);
 
 router.post('/login' , loginController);
 
