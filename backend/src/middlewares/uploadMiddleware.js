f635579d83aa9651154dcdcecaf6afdc4f3faa32
Serife33 <serifekaragur@gmail.com>
2026-02-02 10:48:17 +0100

    middleware upload

--- backend/src/middlewares/uploadMiddleware.js
+++ backend/src/middlewares/uploadMiddleware.js
@@ -1,3 +1,15 @@
 // =====================================================
 // UPLOAD - MIDDLEWARE
 // =====================================================
+
+const multer = require('multer'); 
+const path = require('paht'); 
+
+// configuration du stockage 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let folder = 'uploads/';
+
+
+    }
+})
