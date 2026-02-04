9f193e906f492e7018a65c243ab28262ac691108
Serife33 <serifekaragur@gmail.com>
2026-02-04 09:15:38 +0100

    participation ok

--- backend/src/middlewares/uploadMiddleware.js
+++ backend/src/middlewares/uploadMiddleware.js
@@ -29,11 +29,14 @@ const storage = multer.diskStorage({
 
     // définition du nom du fichier 
     filename: (req, file, cb) => {
+        // on récupère le nom d'origine sans l'extension 
+        const originalName = path.parse(file.originalname).name; 
+        // On nettoie le nom, on remplace les espaces par des tirets 
+        const sanitizedName = originalName.replace(/\s+/g, '-');
         // création d'un suffixe unique (Timestamp actuel + nbre aléatoire entre 1 et 1 milliard)
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         // construction du nom final : fieldname + suffixe + extension originale
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
-
+        cb(null, sanitizedName + '-' + uniqueSuffix + path.extname(file.originalname))
     }
 });
 
