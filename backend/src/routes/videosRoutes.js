2eb68a0ee6c6045a55292ba238e2b202892ad335
PinkMikalai <job.m.sashko@gmail.com>
2026-02-17 09:23:57 +0100

    getVideobyid en cour

--- backend/src/routes/videosRoutes.js
+++ backend/src/routes/videosRoutes.js
@@ -8,12 +8,13 @@ const {
 } = require("../controllers/video/videoController");
 const { createSelectorMemo } = require("../controllers/video/selectorMemoController");
 const optionalAuthMiddleware = require("../middlewares/optionalAuthMiddleware");
-
+const checkRole = require("../middlewares/checkRoleMiddleware");
 const router = Router();
+const authMiddleware = require("../middlewares/authMiddleware");
 
 // nos routes avec les methodes
 router.post("/", createVideo);
-router.post("/:id/memo", createSelectorMemo);
+router.post("/:id/memo",authMiddleware, checkRole("Selector"), createSelectorMemo); //notation de la video par selector
 router.get("/", getAllVideos);
 router.get("/:id", optionalAuthMiddleware, getVideoById);
 router.put("/:id", updateVideo);
